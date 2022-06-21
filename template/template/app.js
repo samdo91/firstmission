7aa56017541e2a5790814d845c7ee7de65758185
samdo91 <fkdlzkfka@naver.com>
2022-06-22 01:25:33 +0900

    자바 테스트 2차 중간

--- template/template/app.js
+++ template/template/app.js
@@ -2,27 +2,34 @@
 
 const toggle = document.querySelector(".toggle")
 const nav = document.querySelector("nav")
-const body =document.querySelector("body")
+const body = document.querySelector("body")
+
+
+
+
 
 if (localStorage.getItem('active') !== null) {
-    nav.classList.add('active')
-    
-  body.classList.add('view')
-} else{
-    body.classList.add('view')
+     nav.classList.add('active')
+     //   body.classList.add('view')
+} else {
+     //  body.classList.add('view')
+}
 
-} 
+body.classList.add('view')
+
 
 toggle.addEventListener('click', togglefuntion)
 
 function togglefuntion() {
-   let antive = document.querySelector('nav.active')
-    if(antive === null)
-    {nav.classList.add('active')
-    localStorage.setItem("active", 1)
-         }
-    
-    else{nav.classList.remove('active')
-    localStorage.removeItem("active");};
+     let antive = document.querySelector('nav.active')
+     if (antive === null) {
+          nav.classList.add('active')
+          localStorage.setItem("active", 1)
+     }
+
+     else {
+          nav.classList.remove('active')
+          localStorage.removeItem("active");
+     };
 }
 
